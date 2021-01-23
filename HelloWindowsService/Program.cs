ec10546a5684cd7459fa6b50d1a9190d9a32aaf4
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-01-23 18:40:04 +0100

    TopShelf Windows Service

---
+++ HelloWindowsService/Program.cs
@@ -0,0 +1,72 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+using Topshelf.Logging;
+
+namespace HelloWindowsService
+{
+    // Install-Package Topshelf
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(configure =>
+            {
+                configure.Service<MySimpleService>(service =>
+                {
+                    // here you can pass dependencies and configuration to the service
+                    service.ConstructUsing(s => new MySimpleService());
+
+                    service.WhenStarted(s => s.Start());
+                    service.WhenStopped(s => s.Stop());
+                });
+
+                configure.StartAutomatically();
+                configure.EnableServiceRecovery(r => r.RestartService(0));
+                configure.RunAsLocalSystem();
+
+                configure.SetServiceName("MySimpleService");
+                configure.SetDisplayName("My Simple Service");
+                configure.SetDescription("This is my simple service");
+
+                // Install-Package Topshelf.Serilog
+                configure.UseSerilog(CreateLogger());
+            });
+        }
+
+        private static ILogger CreateLogger()
+        {
+            // Install-Package Serilog.Sinks.Console -Version 3.1.1
+            // Install-Package Serilog.Sinks.File - Version 4.1.0
+
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("logs/log.txt", Serilog.Events.LogEventLevel.Debug)
+                .CreateLogger();
+            return logger;
+        }
+    }
+
+    public class MySimpleService
+    {
+        private readonly LogWriter logger = HostLogger.Get<MySimpleService>();
+
+        public void Start()
+        {
+            logger.Info("Starting MySimpleService ...");
+            // do some work here
+            Console.WriteLine("Hello from MySimpleService");
+        }
+
+        public void Stop()
+        {
+            logger.Info("Stopping MySimpleService ...");
+            // clean up
+        }
+    }
+}
