d0ba035928ac614ac0e8719e9c5fb5e1f482418c
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-01-23 18:21:13 +0100

    Hosting IIS

---
+++ 11_WebHost/HelloServiceConsoleClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloServiceConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AltkomServiceReference.IHelloService client = new AltkomServiceReference.HelloServiceClient();
+
+            string result = client.Hello("Hello world!");
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 11_WebHost/HelloWebApplication/HelloService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HelloWebApplication
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "HelloService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select HelloService.svc or HelloService.svc.cs at the Solution Explorer and start debugging.
+    public class HelloService : IHelloService
+    {
+        public string Hello(string message)
+        {
+            return message;
+        }
+    }
+}
